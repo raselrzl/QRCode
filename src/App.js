0a2de0ba58a827231f443e28e90cc253fc37535f
Rasel Miah <rasel6041@gmail.com>
2022-05-15 06:15:15 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import QRCode from 'qrcode';
+import { useEffect, useState } from 'react';
+
+function App() {
+  const text="Dirty mind is far better than having a dirty heart!"
+  const [src, setSrc]=useState('')
+
+  useEffect(()=>{
+    QRCode.toDataURL(text)
+          .then((data)=>{
+            setSrc(data);
+          });
+  },[])
+  return (
+    <div className="App">
+       <img src={src}/>
+    </div>
+  );
+}
+
+export default App;
